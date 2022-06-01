2628892dbef0c6746a403b5a5796b0be10ea7682
PedroMarineli <phmo.1606@gmail.com>
2022-06-01 17:50:41 +0000

    hjhj

--- springweb/src/main/java/springmvc/controllers/HomeController.java
+++ springweb/src/main/java/springmvc/controllers/HomeController.java
@@ -1,21 +1,22 @@
 package springmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String index() {
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("/tabuada")
-    public tabuada() {
+    public String tabuada(Model model) {
         int[] resultado = new int[10];
         for(int pos = 0;pos < 10;pos++) {
             resultado[pos] = (pos + 1) * 2;
         }
-        model.addAtribute("resultado", resultado)
-        return "tabuada.jsp";
+        model.addAttribute("resultado", resultado);
+        return "tabuada";
     }
 }
