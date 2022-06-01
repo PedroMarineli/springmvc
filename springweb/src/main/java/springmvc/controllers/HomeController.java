2403be423ce179c8226e01343182070c3fd905c2
PedroMarineli <phmo.1606@gmail.com>
2022-06-01 17:18:33 +0000

    hhghg

--- springweb/src/main/java/springmvc/controllers/HomeController.java
+++ springweb/src/main/java/springmvc/controllers/HomeController.java
@@ -8,4 +8,14 @@ public class HomeController {
     public String index() {
         return "index.jsp";
     }
+
+    @RequestMapping("/tabuada")
+    public tabuada() {
+        int[] resultado = new int[10];
+        for(int pos = 0;pos < 10;pos++) {
+            resultado[pos] = (pos + 1) * 2;
+        }
+        model.addAtribute("resultado", resultado)
+        return "tabuada.jsp";
+    }
 }
