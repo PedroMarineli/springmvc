1f2af6e9ce76100af9b7b95255c6a491c4addbb7
PedroMarineli <phmo.1606@gmail.com>
2022-06-06 17:46:34 +0000

    fjfgj

--- springweb/src/main/java/springmvc/models/Tabuada.java
+++ springweb/src/main/java/springmvc/models/Tabuada.java
@@ -1,15 +1,23 @@
 package springmvc.models;
 
 public class Tabuada {
-    private int multiplicador;
-    private int[] resultados = new int[10];
+    private int multiplicador = 0;
+    private int[] resultados;
 
-    public void setMultiplicador(int multiplicador) {
-        this.multiplicador = multiplicador;
-    }
     public int getMultiplicador() {
         return multiplicador;
     }
 
-    public int[] getResultados
+    public int[] getResultados() {
+        if(!(this.resultados instanceof int[])){
+            for(int num = 1; num <= 10;num++) {
+                this.resultados[num-1] = num * this.multiplicador;
+            }
+        }
+        return this.resultados;
+    }
+
+    public Tabuada(int multiplicador) {
+        this.multiplicador = multiplicador;
+    }
 }
