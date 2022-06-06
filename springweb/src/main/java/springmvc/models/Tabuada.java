acea97cd48cc8c72adc97399d4528c35a2d9ec0c
PedroMarineli <phmo.1606@gmail.com>
2022-06-06 16:57:18 +0000

    kmfjmf

---
+++ springweb/src/main/java/springmvc/models/Tabuada.java
@@ -0,0 +1,15 @@
+package springmvc.models;
+
+public class Tabuada {
+    private int multiplicador;
+    private int[] resultados = new int[10];
+
+    public void setMultiplicador(int multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+    public int getMultiplicador() {
+        return multiplicador;
+    }
+
+    public int[] getResultados
+}
